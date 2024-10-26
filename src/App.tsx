c41bbf7a47279ec384e3f154c577ec153e18fa40
Brandon Nuñez <brandondevnunez@gmail.com>
2024-10-26 10:39:05 -0500

    2era version tienda-carrito

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Guitar from "./components/Guitar"
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Main from "./components/Main"
 import { useCart } from "./hooks/useCart";
 function App() {
   const {
@@ -16,7 +17,8 @@ function App() {
 
   return (
     <>
-      <Header 
+      <Header/>
+      <Main 
         cart={cart}
         removeFromCart={removeFromCart}
         increaseQuantity={increaseQuantity}
@@ -38,13 +40,82 @@ function App() {
           ))}
         </div>
       </main>
-
-
-      <footer className="bg-dark mt-5 py-5">
-        <div className="container-xl">
-          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+      
+    <section className="cursos">
+        <div className="cursos__contenedor cursos__grid">
+            <div className="cursos__contenido">
+                <h2 className="cursos__heading">Aprende a tocar guitarra</h2>
+                <p className="cursos__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque enim fugiat nisi inventore laborum facilis cum reiciendis vel totam velit molestias, animi, odit, architecto officia. Dicta illum exercitationem voluptatem laudantium.</p>
+                <a className="cursos__enlace" href="#">Más información</a>
+            </div>
         </div>
-      </footer>
+    </section>
+
+    <section className="blog">
+        <h2 className="blog__heading">Nuestro Blog</h2>
+        <div className="blog__contenedor blog__grid">
+            <article className="entrada">
+                <img className="entrada__imagen" src="blog_1.jpg" alt="imagen blog"/>
+
+                <div className="entrada__contenido">
+                    <h3 className="entrada__titulo">¿Como elegir tu primer guitarra?</h3>
+                    <p className="entrada__fecha">20 de Enero de 2022</p>
+                    <p className="entrada__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus laborum, quas assumenda, dolorem placeat quo, aliquam labore incidunt alias molestiae maxime quaerat distinctio numquam quam voluptatum. Beatae, molestiae vitae?</p>
+
+                    <a className='entrada__enlace' href='/entrada'>Leer Entrada</a>
+                </div>
+            </article>
+
+            <article className="entrada">
+                <img className="entrada__imagen" src="blog_2.jpg" alt="imagen blog"/>
+
+                <div className="entrada__contenido">
+                    <h3 className="entrada__titulo">¿Como elegir tu primer guitarra?</h3>
+                    <p className="entrada__fecha">20 de Enero de 2022</p>
+                    <p className="entrada__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus laborum, quas assumenda, dolorem placeat quo, aliquam labore incidunt alias molestiae maxime quaerat distinctio numquam quam voluptatum. Beatae, molestiae vitae?</p>
+
+                    <a className='entrada__enlace' href='/entrada'>Leer Entrada</a>
+                </div>
+            </article>
+
+            <article className="entrada">
+                <img className="entrada__imagen" src="blog_3.jpg" alt="imagen blog"/>
+
+                <div className="entrada__contenido">
+                    <h3 className="entrada__titulo">¿Como elegir tu primer guitarra?</h3>
+                    <p className="entrada__fecha">20 de Enero de 2022</p>
+                    <p className="entrada__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus laborum, quas assumenda, dolorem placeat quo, aliquam labore incidunt alias molestiae maxime quaerat distinctio numquam quam voluptatum. Beatae, molestiae vitae?</p>
+
+                    <a className='entrada__enlace' href='/entrada'>Leer Entrada</a>
+                </div>
+            </article>
+
+            <article className="entrada">
+                <img className="entrada__imagen" src="blog_4.jpg" alt="imagen blog"/>
+
+                <div className="entrada__contenido">
+                    <h3 className="entrada__titulo">¿Como elegir tu primer guitarra?</h3>
+                    <p className="entrada__fecha">20 de Enero de 2022</p>
+                    <p className="entrada__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio delectus laborum, quas assumenda, dolorem placeat quo, aliquam labore incidunt alias molestiae maxime quaerat distinctio numquam quam voluptatum. Beatae, molestiae vitae?</p>
+
+                    <a className='entrada__enlace' href='/entrada'>Leer Entrada</a>
+                </div>
+            </article>
+        </div>
+    </section>
+
+    <footer className="footer">
+        <div className="footer__contenedor footer__contenido">
+            <nav className="footer__nav">
+                <a className='footer__enlace' href='/'>Inicio</a>
+                <a className='footer__enlace' href='/nosotros'>Nosotros</a>
+                <a className='footer__enlace' href='/blog'>Blog</a>
+                <a className='footer__enlace' href='/tienda'>Tienda</a>
+            </nav>
+
+            <p className="footer__copyright">Todos los derechos reservados</p>
+        </div>
+    </footer>
     </>
   )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,106 +1,35 @@
-import type { CartItem, Guitar } from "../types"
-type Header = {
-    cart: CartItem[]
-    removeFromCart: (id: Guitar["id"]) => void
-    increaseQuantity: (id: Guitar["id"]) => void
-    drecrementQuantity: (id: Guitar["id"]) => void
-    clearCart: () => void
-    isEmpty: boolean
-    cartTotal: number
-}
-
-export default function Header({cart,
-    removeFromCart,
-    increaseQuantity,
-    drecrementQuantity,
-    clearCart, 
-    isEmpty, 
-    cartTotal
-}: Header) {
-
+export default function Header() {
     return (
-        <header className="py-5 header">
-            <div className="container-xl">
-                <div className="row justify-content-center justify-content-md-between">
-                    <div className="col-8 col-md-3">
-                        <a href="index.html">
-                            <img className="img-fluid" src="logo.svg" alt="imagen logo" />
-                        </a>
-                    </div>
-                    <nav className="col-md-6 a mt-5 d-flex align-items-start justify-content-end">
-                        <div
-                            className="carrito"
-                        >
-                            <img className="img-fluid" src="carrito.png" alt="imagen carrito" />
+        <header className="header">
+            <div className="header__contenedor">
+                <div className="header__barra">
+                    <a href="/">
+                        <img className="header__logo" src="logo.svg" alt="imagen logo"/>
+                    </a>
 
-                            <div id="carrito" className="bg-white p-3">
-                                {isEmpty ? (
-                                     <p className="text-center">El carrito esta vacio</p>
-                                ):(
-                                <>
-                                <table className="w-100 table">
-                                    <thead>
-                                        <tr>
-                                            <th>Imagen</th>
-                                            <th>Nombre</th>
-                                            <th>Precio</th>
-                                            <th>Cantidad</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {cart.map(guitar =>(
-                                                <tr key={guitar.id}>
-                                                    <td>
-                                                        <img className="img-fluid" src={`${guitar.image}.jpg`} alt="imagen guitarra" />
-                                                    </td>
-                                                    <td>{guitar.name}</td>
-                                                    <td className="fw-bold">
-                                                        {guitar.price}
-                                                    </td>
-                                                    <td className="flex align-items-start gap-4">
-                                                        <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                            onClick={()=> drecrementQuantity(guitar.id)}
-                                                        >
-                                                            -
-                                                        </button>
-                                                        {guitar.quantity}
-                                                        <button
-                                                            type="button"
-                                                            className="btn btn-dark"
-                                                            onClick={()=> increaseQuantity(guitar.id)}
-                                                        >
-                                                            +
-                                                        </button>
-                                                    </td>
-                                                    <td>
-                                                        <button
-                                                            className="btn btn-danger"
-                                                            type="button"
-                                                            onClick={()=> removeFromCart(guitar.id)}
-                                                        >
-                                                            X
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                                <p className="text-end">Total pagar: <span className="fw-bold">${cartTotal}</span></p>
-                                </>
-                            )}
-
-                                <button 
-                                    className="btn btn-dark w-100 mt-3 p-2"
-                                    onClick={clearCart}
-                                >Vaciar Carrito</button>
-                            </div>
-                        </div>
+                    <nav className="navegacion">
+                        <a className='navegacion__enlace' href='/'>Inicio</a>
+                        <a className='navegacion__enlace' href='/nosotros'>Nosotros</a>
+                        <a className='navegacion__enlace' href='/blog'>Blog</a>
+                        <a className='navegacion__enlace' href='/tienda'>Tienda</a>
                     </nav>
                 </div>
+
+                <div className="modelo">
+                    <h1 className="modelo__nombre">Modelo VAI</h1>
+                    <p className="modelo__descripcion">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae, dolorum voluptatem fuga illum nisi saepe, repellat laudantium accusantium doloremque harum</p>
+                    <p className="modelo__precio">$399</p>
+                    <a className='modelo__enlace' href='/producto'>Ver Producto</a>
+                </div>
+            </div>
+
+            <img className="header__guitarra" src="header_guitarra.png" alt="imagen header guitarra"/>
+            <div>
+                <a className="logo-enlace" href="https://wa.me/51978069398">
+                    <img className="wsp-logo" src="logo-ws.png" alt="wsp"/>
+                </a>
             </div>
         </header>
+
     )
 }
